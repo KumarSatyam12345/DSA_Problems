ec673e722b376e17748b64dadcd124a27565eae6
satyam <sk95232696@gmail.com>
2025-10-11 20:20:51 +0530

    bubble sort

--- src/sortingAlgo/BubbleSort.java
+++ src/sortingAlgo/BubbleSort.java
@@ -2,6 +2,26 @@ package sortingAlgo;
 
 public class BubbleSort {
     public static void main(String[] args) {
+        int[] arr = {5, 3, 8, 4, 2};
+        bubbleSort(arr);
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+    }
 
+    private static void bubbleSort(int[] arr) {
+        int n = arr.length;
+        for (int i = 0; i < n - 1; i++) {
+            boolean swapped = false;
+            for (int j = 0; j < n - 1 - i; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                    swapped = true;
+                }
+            }
+            if (!swapped) break;
+        }
     }
 }
