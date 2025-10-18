cd60c53e89fefe1df705258c4455119fc5321936
satyam <sk95232696@gmail.com>
2025-10-18 22:50:11 +0530

    insertion sort

--- src/sortingAlgo/SelectionSort.java
+++ src/sortingAlgo/SelectionSort.java
@@ -1,4 +1,26 @@
 package sortingAlgo;
 
 public class SelectionSort {
+    public static void main(String[] args) {
+        int[] arr = {5, 3, 8, 4, 2};
+        selectionSort(arr);
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+    }
+
+    private static void selectionSort(int[] arr) {
+        int n = arr.length;
+        for (int i = 0; i < n - 1; i++) {
+            int minIdx = i;
+            for (int j = i + 1; j < n; j++) {
+                if (arr[j] < arr[minIdx]) {
+                    minIdx = j;
+                }
+            }
+            int temp = arr[minIdx];
+            arr[minIdx] = arr[i];
+            arr[i] = temp;
+        }
+    }
 }
